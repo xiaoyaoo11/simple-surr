efa78a3e046d9b1efb0886fe47b90cf902c70bfc
xiaoyaoo11 <taitran411789@gmail.com>
2025-01-31 11:11:37 +0700

    test

--- lua/simple-surr/config.lua
+++ lua/simple-surr/config.lua
@@ -1,17 +1,17 @@
 local M = {}
 
 M.setup_keymaps = function()
-	vim.keymap.set("v", "<leader>ss", function()
+	vim.keymap.set("v", "<leader>sv", function()
 		local style = vim.fn.input("Enter surround style (e.g., [, {, (, }, ', \", `, custom): ")
 		require("simple-surr.surround").surround_selection(style)
 	end, { desc = "Surround selection with custom or predefined style" })
 
-	vim.keymap.set("n", "<leader>sr", function()
+	vim.keymap.set("n", "<leader>sw", function()
 		local style = vim.fn.input("Enter surround style (e.g., [, {, (, }, ', \", `, custom): ")
 		require("simple-surr.surround").surround_word(style)
 	end, { desc = "Surround word under cursor with custom or predefined style" })
 
-	vim.keymap.set("n", "<leader>sc", function()
+	vim.keymap.set("n", "<leader>sr", function()
 		local change = vim.fn.input("Change surround style (leave empty to remove): ")
 		if change == "" then
 			require("simple-surr.surround").remove_or_change_surround_word()
